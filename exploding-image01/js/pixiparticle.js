66f0ad717ed992929bb83bd1f70e27d324d40791
blindfish3 <ben@blindfish.co.uk>
2015-12-07 22:10:35 +0000

    tidying and finessing

---
+++ exploding-image01/js/pixiparticle.js
@@ -0,0 +1,64 @@
+
+blindfish.PixiParticle = function (s) {
+  // The pixi sprite that does all the heavy lifting :)
+  this.s  = s;
+  //store the original position in the image for this particle
+  this.originX = s.position.x;
+  this.originY = s.position.y;
+
+  this.vx = 0;
+  this.vy = 0;
+
+}
+
+blindfish.PixiParticle.prototype.move = function (mouseX, mouseY, mouseOverStage) {
+
+    var dx = mouseX - this.s.position.x,
+          dy = mouseY - this.s.position.y,
+          deltaSquared = dx * dx + dy * dy,
+          angle = Math.atan2(dy, dx),
+          speed = -deltaSquared;
+
+
+      if(mouseOverStage){
+         if (deltaSquared < 2400) {
+                this.returnToOrigin = false;
+                this.vx = Math.cos(angle) * speed;
+                this.vy = Math.sin(angle) * speed;
+            }
+        }
+    // a value double of that above appears to work best here
+        if (deltaSquared > 4800) {
+                this.moveToOrigin();
+            }
+
+    this.s.position.x += this.vx;
+    this.s.position.y += this.vy;
+
+}
+
+blindfish.PixiParticle.prototype.moveToOrigin = function () {
+     if (this.returnToOrigin) {
+        this.s.position.x = this.originX;
+        this.s.position.y = this.originY;
+    } else {
+        // stop the initially applied motion in readiness for
+        // springing back to origin...
+        this.vx = 0;
+        this.vy = 0;
+
+        var dx2 = this.originX - this.s.position.x,
+               dy2 = this.originY - this.s.position.y;
+
+        this.vx += dx2 * blindfish.g.spring;
+        this.vy += dy2 * blindfish.g.spring;
+        this.vx *= blindfish.g.friction;
+        this.vy *= blindfish.g.friction;
+
+        // ensure particles do eventually get back to their exact start position
+        if (Math.abs(dx2) < 0.05 && Math.abs(dy2) < 0.05) {
+            this.returnToOrigin = true;
+        }
+
+    }
+}
--- exploding-image01/js/sketch.js
+++ exploding-image01/js/sketch.js
@@ -1,89 +1,87 @@
-(function() {
+(function () {
 
 
-	// create a renderer instance.
-	var renderer = PIXI.autoDetectRenderer(450, 325);
+    // create a renderer instance.
+    blindfish.renderer = PIXI.autoDetectRenderer(450, 325);
 
-	// add the renderer view element to the DOM
-	document.getElementById('sketch01').appendChild(renderer.view);
+    // add the renderer view element to the DOM
+    document.getElementById('sketch01').appendChild(blindfish.renderer.view);
 
-
-
+    // possibly unecessary given we're using a single image
     PIXI.loader
         .add('elephant.jpg')
         .load(onAssetsLoaded);
 
-    function onAssetsLoaded() {
 
+    function onAssetsLoaded() {
 
         blindfish.stage = new PIXI.Container();
 
+        blindfish.container = new PIXI.DisplayObjectContainer();
+        blindfish.container = new PIXI.ParticleContainer(200000, [false, true, false, false, false]);
+        blindfish.stage.addChild(blindfish.container);
+        blindfish.sprites = [];
+
         var img = PIXI.BaseTexture.fromImage('elephant.jpg'),
-               imgWidth = img.width,
-               imgHeight = img.height,
-               particleWidth = 10,
-               particleHeight = 5,
-               halfParticleWidth = particleWidth/2,
-               halfParticleHeight = particleHeight/2,
-               horizontalDivisions = Math.floor(imgWidth / particleWidth),
-               verticalDivisions = Math.floor(imgHeight / particleHeight),
-               sprites = [];
+            imgWidth = img.width,
+            imgHeight = img.height,
+            particleWidth = 6,
+            particleHeight = 6,
+            halfParticleWidth = particleWidth / 2,
+            halfParticleHeight = particleHeight / 2,
+            horizontalDivisions = Math.floor(imgWidth / particleWidth),
+            verticalDivisions = Math.floor(imgHeight / particleHeight),
+            index = 0;
 
+        for (var i = 0; i < verticalDivisions; i++) {
+            for (var j = 0; j < horizontalDivisions; j++) {
 
-            for(var i=0; i<verticalDivisions; i++){
-                for(var j=0; j<horizontalDivisions; j++){
-                    (function() {
-                    var index = j * particleWidth + i,
-                          x = j * particleWidth,
-                          y = i * particleHeight,
-                        texture = new PIXI.Texture(img, new    PIXI.Rectangle(x,y,particleWidth,particleHeight)),
-                        s = new PIXI.Sprite(texture);
-                       s.anchor.x = 0.5;
-                        s.anchor.y = 0.5;
-                        s.position.x = x - halfParticleWidth,
-                        s.position.y = y - halfParticleHeight;
+                var x = j * particleWidth,
+                    y = i * particleHeight,
+                    texture = new PIXI.Texture(img, new PIXI.Rectangle(x, y, particleWidth, particleHeight)),
+                    s = new PIXI.Sprite(texture);
 
-                     sprites[index] = new blindfish.PixiParticle(s);
+                s.anchor.x = 0.5;
+                s.anchor.y = 0.5;
+                s.position.x = x - halfParticleWidth;
+                s.position.y = y - halfParticleHeight;
 
-                    blindfish.stage.addChild(s);
+                blindfish.sprites[index] = new blindfish.PixiParticle(s);
 
-                    })();
-                }
+                blindfish.container.addChild(s);
+                index++;
             }
+        }
 
+        blindfish.spritesLen = blindfish.sprites.length;
+        console.info("number of particles: " + blindfish.spritesLen);
 
         requestAnimationFrame(animate);
 
     }
 
 
+    function animate() {
+        // avoid recalculating these in every particle!
+        var mouseX = blindfish.renderer.plugins.interaction.mouse.global.x,
+            mouseY = blindfish.renderer.plugins.interaction.mouse.global.y,
+            mouseOverStage = false;
 
-	function animate() {
+        if (mouseX > 0 && mouseX < blindfish.renderer.width && mouseY > 0 && mouseY < blindfish.renderer.height) {
+            mouseOverStage = true;
+        }
+        else {
+            mouseOverStage = false;
+        }
 
-	    requestAnimationFrame(animate);
+        for (var i = blindfish.spritesLen - 1; i > 0; i--) {
+            blindfish.sprites[i].move(mouseX, mouseY, mouseOverStage);
+        }
 
-	    // render the stage
-	    renderer.render(blindfish.stage);
-	}
+        // render the stage
+        blindfish.renderer.render(blindfish.stage);
+        requestAnimationFrame(animate);
+    }
 
 
 })();
-
-blindfish.PixiParticle = function (args) {
-  this.x = args.x;
-  this.y = args.y;
-  this.vx = args.vx || 0;
-  this.vy = args.vy || 0;
-  this.size = args.size;
-  this.img = args.img;
-
-   // this.imgWidth = this.img.width;
- //   this.imgHeight = this.img.height;
-
-//    this.centreX = this.x + this.size / 2;
-//    this.centreY = this.y + this.size / 2;
-//    this.originX = this.x;
-//    this.originY = this.y;
-//    this.doGoToOrigin = true;
-
-}
